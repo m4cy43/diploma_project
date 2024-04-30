2e381c2b74a9145a299ad16ec79d8c2cba10b50a
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2024-04-30 21:42:37 +0300

    user controller all (raw)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,10 +3,11 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const Role = require("../models/roleModel");
+const { Op } = require("sequelize");
 
-// @desc Create new user
-// @route POST /api/user/register
-// @access Public
+// @desc    Create new user
+// @route   POST /api/user/register
+// @access  Public
 const createUser = asyncHandler(async (req, res) => {
   const { email, password, name, surname, middlename, phone } = req.body;
 
@@ -41,7 +42,7 @@ const createUser = asyncHandler(async (req, res) => {
   });
   if (user) {
     // Add role to user
-    const role = await Role.findOrCreate({ where: { role: "Unverified" } });
+    const role = await Role.findOrCreate({ where: { role: "unverified" } });
     await user.addRole(role[0]);
 
     res.status(201).json({
@@ -61,9 +62,9 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Login user
-// @route POST /api/user/login
-// @access Public
+// @desc    Login user
+// @route   POST /api/user/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -93,55 +94,307 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc Verify the user
-// @route POST /api/user/verify/{uuid}
-// @access Private
-const verifyUser = asyncHandler(async (req, res) => {
-  if (!req.user.roles.includes("Worker")) {
-    res.status(401);
-    throw new Error("Action not alowed due to role");
-  }
-
-  let user = await User.findByPk(req.params.uuid);
-  // Check the user exists
-  if (!user) {
-    res.status(400);
-    throw new Error("There is no such user");
-  }
-
-  const newrole = await Role.findOrCreate({ where: { role: "Verified" } });
-  await user.addRole(newrole[0]);
-
-  res.status(204).json();
-});
-
-// @desc Set worker rights
-// @route POST /api/user/worker/{uuid}
-// @access Private
-const setWorker = asyncHandler(async (req, res) => {
-  if (!req.user.roles.includes("Admin")) {
-    res.status(401);
-    throw new Error("Action not alowed due to role");
-  }
-
-  let user = await User.findByPk(req.params.uuid);
-  // Check the user exists
-  if (!user) {
-    res.status(400);
-    throw new Error("There is no such user");
-  }
-
-  const newrole = await Role.findOrCreate({ where: { role: "Worker" } });
-  await user.addRole(newrole[0]);
-
-  res.status(204).json();
-});
-
-// @desc Check auth user
-// @route GET /api/user/authuser
-// @access Private
+// @desc    Return auth user
+// @route   GET /api/user/authuser
+// @access  Private
 const getAuthUser = asyncHandler(async (req, res) => {
   res.status(200).json(req.user);
+});
+
+// @desc    Verify the user
+// @route   PUT /api/user/verify/{uuid}
+// @access  Private
+const verifyUser = asyncHandler(async (req, res) => {
+  if (!req.user.roles.includes("admin")) {
+    res.status(401);
+    throw new Error("Action not alowed due to role");
+  }
+
+  let user = await User.findByPk(req.params.uuid);
+  // Check the user exists
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  const newrole = await Role.findOrCreate({ where: { role: "verified" } });
+  await user.addRole(newrole[0]);
+
+  res.status(204).json();
+});
+
+// @desc    Set admin rights
+// @route   PUT /api/user/setadmin/{uuid}
+// @access  Private
+const setAdmin = asyncHandler(async (req, res) => {
+  if (!req.user.roles.includes("super")) {
+    res.status(401);
+    throw new Error("Action not alowed due to role");
+  }
+
+  let user = await User.findByPk(req.params.uuid);
+  // Check the user exists
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  const newrole = await Role.findOrCreate({ where: { role: "admin" } });
+  await user.addRole(newrole[0]);
+
+  res.status(204).json();
+});
+
+// @desc    Set superadmin rights
+// @route   PUT /api/user/setsuper/{uuid}
+// @access  Private
+const setSuperAdmin = asyncHandler(async (req, res) => {
+  if (!req.user.roles.includes("super")) {
+    res.status(401);
+    throw new Error("Action not alowed due to role");
+  }
+
+  let user = await User.findByPk(req.params.uuid);
+  // Check the user exists
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  const newrole = await Role.findOrCreate({ where: { role: "super" } });
+  await user.addRole(newrole[0]);
+
+  res.status(204).json();
+});
+
+// @desc    Delete auth user
+// @route   DELETE /api/user/
+// @access  Private
+const deleteMe = asyncHandler(async (req, res) => {
+  let user = await User.findByPk(req.user.uuid);
+  // Check the user exists
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  await user.destroy();
+  res.status(204).json();
+});
+
+// @desc    Delete the user
+// @route   DELETE /api/user/{uuid}
+// @access  Public
+const deleteUser = asyncHandler(async (req, res) => {
+  let user = await User.findByPk(req.params.uuid);
+  // Check the user exists
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  await user.destroy();
+  res.status(204).json();
+});
+
+// @desc    Update auth user
+// @route   PUT /api/user/
+// @access  Private
+const updateMe = asyncHandler(async (req, res) => {
+  const { email, password, name, surname, middlename, phone } = req.body;
+  // Check the value
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Required values are missing");
+  }
+
+  // Check the user exists
+  let user = await User.findByPk(req.user.uuid);
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  // Check if user exists by email
+  const emailExists = await User.findOne({ where: { email } });
+  if (emailExists && user.email !== email) {
+    res.status(400);
+    throw new Error("User with such email already exists");
+  }
+  // Check if phone exists
+  if (phone) {
+    const phoneExists = await User.findOne({ where: { phone } });
+    if (phoneExists && user.phone !== phone) {
+      res.status(400);
+      throw new Error("User with such phone already exists");
+    }
+  }
+
+  // Encryption & hashing
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password.toString(), salt);
+
+  user.set({
+    email: email,
+    password: hash,
+    // membership: membership,
+    name: name,
+    surname: surname,
+    middlename: middlename,
+    phone: phone,
+  });
+
+  await user.save();
+  await user.reload();
+  if (user) {
+    res.status(201).json({
+      uuid: user.uuid,
+      email: user.email,
+      membership: user.membership,
+      name: user.name,
+      surname: user.surname,
+      middlename: user.middlename,
+      phone: user.phone,
+      token: generateJWT(user.uuid),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc    Update the user
+// @route   PUT /api/user/{uuid}
+// @access  Public
+const updateUser = asyncHandler(async (req, res) => {
+  const { email, password, name, surname, middlename, phone } = req.body;
+  // Check the value
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Required values are missing");
+  }
+
+  // Check the user exists
+  let user = await User.findByPk(req.params.uuid);
+  if (!user) {
+    res.status(400);
+    throw new Error("There is no such user");
+  }
+
+  // Check if user exists by email
+  const emailExists = await User.findOne({ where: { email } });
+  if (emailExists && user.email !== email) {
+    res.status(400);
+    throw new Error("User with such email already exists");
+  }
+  // Check if phone exists
+  if (phone) {
+    const phoneExists = await User.findOne({ where: { phone } });
+    if (phoneExists && user.phone !== phone) {
+      res.status(400);
+      throw new Error("User with such phone already exists");
+    }
+  }
+
+  // Encryption & hashing
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password.toString(), salt);
+
+  user.set({
+    email: email,
+    password: hash,
+    // membership: membership,
+    name: name,
+    surname: surname,
+    middlename: middlename,
+    phone: phone,
+  });
+
+  await user.save();
+  await user.reload();
+  if (user) {
+    res.status(201).json({
+      uuid: user.uuid,
+      email: user.email,
+      membership: user.membership,
+      name: user.name,
+      surname: user.surname,
+      middlename: user.middlename,
+      phone: user.phone,
+      token: generateJWT(user.uuid),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc    Find user by id
+// @route   GET /api/user/{uuid}
+// @access  Public
+const findByUuid = asyncHandler(async (req, res) => {
+  const user = await User.findByPk(req.params.uuid, {
+    attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    include: [
+      { model: Role, attributes: ["role"], through: { attributes: [] } },
+    ],
+  });
+  if (user) {
+    res.status(200).json({
+      uuid: user.uuid,
+      email: user.email,
+      membership: user.membership,
+      name: user.name,
+      surname: user.surname,
+      middlename: user.middlename,
+      phone: user.phone,
+      roles: user.roles.map((x) => x.role),
+    });
+  } else {
+    res.status(400);
+    throw new Error("User do not exist");
+  }
+});
+
+// @desc    Find users by membership
+// @route   GET /api/user/q?query=&role=
+// @access  Public
+const findByMembershipAndRole = asyncHandler(async (req, res) => {
+  const { query, role } = req.query;
+  if (!query || !role) {
+    res.status(400);
+    throw new Error("Missing the query");
+  }
+
+  const users = await User.findAll({
+    attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    include: [
+      { model: Role, through: { attributes: [] }, where: { role: role } },
+    ],
+    where: { membership: { [Op.substring]: query } },
+    order: [["UpdatedAt", "DESC"]],
+  });
+  res.status(200).json(users);
+});
+
+// @desc    Find all users by role
+// @route   GET /api/user/role?role=
+// @access  Public
+const findByRole = asyncHandler(async (req, res) => {
+  const { role } = req.query;
+  if (!role) {
+    res.status(400);
+    throw new Error("Missing the query");
+  }
+
+  const users = await User.findAll({
+    attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    include: [
+      { model: Role, through: { attributes: [] }, where: { role: role } },
+    ],
+    order: [["UpdatedAt", "DESC"]],
+  });
+  res.status(200).json(users);
 });
 
 // Auxiliary function
@@ -165,6 +418,14 @@ module.exports = {
   createUser,
   loginUser,
   getAuthUser,
-  setWorker,
   verifyUser,
+  setAdmin,
+  setSuperAdmin,
+  deleteMe,
+  deleteUser,
+  updateMe,
+  updateUser,
+  findByUuid,
+  findByMembershipAndRole,
+  findByRole,
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,8 +4,16 @@ const {
   createUser,
   loginUser,
   getAuthUser,
-  setWorker,
   verifyUser,
+  setAdmin,
+  setSuperAdmin,
+  deleteMe,
+  deleteUser,
+  updateMe,
+  updateUser,
+  findByUuid,
+  findByMembershipAndRole,
+  findByRole,
 } = require("../controllers/userController");
 
 // Auth middleware
@@ -16,12 +24,28 @@ const { authorization } = require("../middleware/authMiddleware");
 router.post("/register", createUser);
 // POST /api/user/login
 router.post("/login", loginUser);
-// PUT /api/user/setworker
-router.put("/worker/:uuid", authorization, setWorker);
-// PUT /api/user/setworker
+// PUT /api/user/setadmin/{uuid}
+router.put("/setadmin/:uuid", authorization, setAdmin);
+// PUT /api/user/setsuper/{uuid}
+router.put("/setsuper/:uuid", authorization, setSuperAdmin);
+// PUT /api/user/verify/{uuid}
 router.put("/verify/:uuid", authorization, verifyUser);
+// DELETE /api/user/
+router.delete("/", authorization, deleteMe);
+// DELETE /api/user/{uuid}
+router.delete("/:uuid", deleteUser);
+// PUT /api/user/
+router.put("/", authorization, updateMe);
+// PUT /api/user/{uuid}
+router.put("/:uuid", updateUser);
+// GET /api/user/{uuid}
+router.get("/get/:uuid", findByUuid);
+// GET /api/user/q?query=&role=
+router.get("/q", findByMembershipAndRole);
+// GET /api/user/role?role=
+router.get("/role", findByRole);
 
-// GET /api/user/authuser
-router.get("/authuser", authorization, getAuthUser);
+// GET /api/user/me
+router.get("/me", authorization, getAuthUser);
 
 module.exports = router;
